91343677372d95fc9e21ad1462eb11511b949fd1
Enric Tejedor Saavedra <enric.tejedor.saavedra@cern.ch>
2015-12-16 18:38:35 +0100

    Setup the environment via CVMFS

--- systemuser.sh
+++ systemuser.sh
@@ -10,7 +10,16 @@ LONGNAME=/home/"$USER"/MyCERNBox
 ln -nfs /eos/user/"$FIRSTLETTER"/"$USER" $LONGNAME
 chown -h $USER $LONGNAME
 
-sudo -E -u $USER jupyterhub-singleuser \
+echo "Setting up environment from CVMFS"
+SETUP_FILE=$ROOT_LCG_VIEW_PATH/$ROOT_LCG_VIEW_NAME/$ROOT_LCG_VIEW_PLATFORM/setup.sh
+source $SETUP_FILE
+echo "Using setup file: $SETUP_FILE"
+env
+
+# Force inheritance of PYTHONPATH and LD_LIBRARY_PATH
+alias sudoenv="sudo LD_LIBRARY_PATH=$LD_LIBRARY_PATH PYTHONPATH=$PYTHONPATH"
+
+sudoenv -E -u $USER jupyterhub-singleuser \
   --port=8888 \
   --ip=0.0.0.0 \
   --user=$JPY_USER \
