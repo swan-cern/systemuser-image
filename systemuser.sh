b343240b318b2746f6ad3d2aa7094298004a0cfb
dpiparo <danilo.piparo@cern.ch>
2016-03-03 08:26:38 +0100

    Optimise disk access and add debug timing
    o Remove look and feel customisation
    o Write the environment file without opening the file 3 times but just one

--- systemuser.sh
+++ systemuser.sh
@@ -7,38 +7,48 @@
 
 # Create notebook user
 # The $HOME directory is specified upstream in the Spawner
+echo "START UserId " `date`
 echo "Creating user $USER ($USER_ID)"
 useradd -u $USER_ID -s $SHELL -d $HOME $USER
+echo "STOP UserId " `date`
 
 # Setup the LCG View on CVMFS
 echo "Setting up environment from CVMFS"
 export LCG_VIEW=$ROOT_LCG_VIEW_PATH/$ROOT_LCG_VIEW_NAME/$ROOT_LCG_VIEW_PLATFORM
 
 # Add ROOT kernel
+echo "START Adding ROOT Kernel " `date`
 echo "Adding ROOT kernel"
 export ETC_NB=$LCG_VIEW/etc/notebook
 JPY_LOCAL_DIR="$HOME"/.local
 export KERNEL_DIR=$JPY_LOCAL_DIR/share/jupyter/kernels
 sudo -E -u $USER mkdir -p $KERNEL_DIR
 sudo -E -u $USER cp -rL $ETC_NB/kernels/root $KERNEL_DIR
+echo "STOP Adding ROOT Kernel " `date`
 
 # Customise look and feel
-echo "Customising the look and feel"
+# Look and feel probably not needed
+#echo "Customising the look and feel"
 export JPY_DIR="$HOME"/.jupyter
-sudo -E -u $USER mkdir -p $JPY_DIR
-sudo -E -u $USER cp -rL $ETC_NB/custom $JPY_DIR
+#sudo -E -u $USER mkdir -p $JPY_DIR
+#sudo -E -u $USER cp -rL $ETC_NB/custom $JPY_DIR
 
 # Set environment for the notebook process
 # The kernels and the terminal will inherit
+echo "START Setting Environment " `date`
 echo "Setting environment"
 export JPY_CONFIG=$JPY_DIR/jupyter_notebook_config.py
-sudo -E -u $USER sh -c 'source $LCG_VIEW/setup.sh && echo "import os"                 >  $JPY_CONFIG ; \
-                        echo "os.environ[\"PATH\"]            = \"$PATH\""            >> $JPY_CONFIG ; \
-                        echo "os.environ[\"LD_LIBRARY_PATH\"] = \"$LD_LIBRARY_PATH\"" >> $JPY_CONFIG ; \
-                        echo "os.environ[\"PYTHONPATH\"]      = \"$PYTHONPATH\""      >> $JPY_CONFIG ;'
+sudo -E -u $USER sh -c 'ls && echo "import os;"\
+                                   "os.environ[\"PATH\"]            = \"$PATH\";"\
+                                   "os.environ[\"LD_LIBRARY_PATH\"] = \"$LD_LIBRARY_PATH\";"\
+                                   "os.environ[\"PYTHONPATH\"]      = \"$PYTHONPATH\""      > $JPY_CONFIG ;'
+
+echo "STOP Setting Environment " `date`
 
 # Overwrite link for python2 in the image
+echo "START Overwriting Python2 link " `date`
 ln -sf $LCG_VIEW/bin/python /usr/local/bin/python2
+echo "STOP Overwriting Python2 link " `date`
 
 # Run notebook server
 echo "Running the notebook server"
