1f891d10f8b28edf8806441cef86994679d6c390
root <root@dmaas-proto-cc7.cern.ch>
2016-03-14 11:56:01 +0100

    Polish the script and correctly preserve the environment

--- systemuser.sh
+++ systemuser.sh
@@ -7,48 +7,42 @@
 
 # Create notebook user
 # The $HOME directory is specified upstream in the Spawner
-echo "START UserId " `date`
 echo "Creating user $USER ($USER_ID)"
 useradd -u $USER_ID -s $SHELL -d $HOME $USER
-echo "STOP UserId " `date`
 
 # Setup the LCG View on CVMFS
 echo "Setting up environment from CVMFS"
 export LCG_VIEW=$ROOT_LCG_VIEW_PATH/$ROOT_LCG_VIEW_NAME/$ROOT_LCG_VIEW_PLATFORM
+source $LCG_VIEW/setup.sh
+echo "Using the following environment:"
+echo "PYTHONPATH: $PYTHONPATH"
+echo "LD_LIBRARY_PATH: $LD_LIBRARY_PATH"
+echo "PATH: $PATH"
 
 # Add ROOT kernel
-echo "START Adding ROOT Kernel " `date`
 echo "Adding ROOT kernel"
-export ETC_NB=$LCG_VIEW/etc/notebook
+ETC_NB=$LCG_VIEW/etc/notebook
 JPY_LOCAL_DIR="$HOME"/.local
-export KERNEL_DIR=$JPY_LOCAL_DIR/share/jupyter/kernels
+KERNEL_DIR=$JPY_LOCAL_DIR/share/jupyter/kernels
 sudo -E -u $USER mkdir -p $KERNEL_DIR
 sudo -E -u $USER cp -rL $ETC_NB/kernels/root $KERNEL_DIR
-echo "STOP Adding ROOT Kernel " `date`
-
-# Customise look and feel
-# Look and feel probably not needed
-#echo "Customising the look and feel"
-export JPY_DIR="$HOME"/.jupyter
-#sudo -E -u $USER mkdir -p $JPY_DIR
-#sudo -E -u $USER cp -rL $ETC_NB/custom $JPY_DIR
 
 # Set environment for the notebook process
 # The kernels and the terminal will inherit
-echo "START Setting Environment " `date`
 echo "Setting environment"
-export JPY_CONFIG=$JPY_DIR/jupyter_notebook_config.py
-sudo -E -u $USER sh -c 'ls && echo "import os;"\
-                                   "os.environ[\"PATH\"]            = \"$PATH\";"\
-                                   "os.environ[\"LD_LIBRARY_PATH\"] = \"$LD_LIBRARY_PATH\";"\
-                                   "os.environ[\"PYTHONPATH\"]      = \"$PYTHONPATH\""      > $JPY_CONFIG ;'
-
-echo "STOP Setting Environment " `date`
+JPY_DIR="$HOME"/.jupyter
+sudo -E -u $USER mkdir -p $JPY_DIR
+JPY_CONFIG=$JPY_DIR/jupyter_notebook_config.py
+JPY_CONFIG_TMP=`mktemp`
+echo "import os"                                           > $JPY_CONFIG_TMP
+echo "os.environ['PATH']            = '$PATH'"            >> $JPY_CONFIG_TMP
+echo "os.environ['LD_LIBRARY_PATH'] = '$LD_LIBRARY_PATH'" >> $JPY_CONFIG_TMP
+echo "os.environ['PYTHONPATH']      = '$PYTHONPATH'"      >> $JPY_CONFIG_TMP
+chmod 644 $JPY_CONFIG_TMP
+sudo -E -u $USER cp $JPY_CONFIG_TMP $JPY_CONFIG
 
 # Overwrite link for python2 in the image
-echo "START Overwriting Python2 link " `date`
 ln -sf $LCG_VIEW/bin/python /usr/local/bin/python2
-echo "STOP Overwriting Python2 link " `date`
 
 # Run notebook server
 echo "Running the notebook server"
@@ -59,4 +53,4 @@ sudo -E -u $USER sh -c 'cd $HOME && jupyterhub-singleuser \
   --cookie-name=$JPY_COOKIE_NAME \
   --base-url=$JPY_BASE_URL \
   --hub-prefix=$JPY_HUB_PREFIX \
-  --hub-api-url=$JPY_HUB_API_URL '
+  --hub-api-url=$JPY_HUB_API_URL'
