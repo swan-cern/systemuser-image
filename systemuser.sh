1dcd0dca0ed7c04e972974f2f4f8d64ebbf0b17a
Diogo Castro <diogo.castro@cern.ch>
2019-04-09 10:11:30 +0200

    Update Jupyter extensions to v7.1 The update includes the new atomic save for notebooks and EOS versions

--- systemuser.sh
+++ systemuser.sh
@@ -51,6 +51,7 @@ echo "c.FileCheckpoints.checkpoint_dir = '$SCRATCH_HOME/.ipynb_checkpoints'"    
 echo "c.NotebookNotary.db_file = '$JUPYTER_LOCAL_PATH/nbsignatures.db'"     >> $JPY_CONFIG
 echo "c.NotebookNotary.secret_file = '$JUPYTER_LOCAL_PATH/notebook_secret'" >> $JPY_CONFIG
 echo "c.NotebookApp.contents_manager_class = 'swancontents.filemanager.swanfilemanager.SwanFileManager'" >> $JPY_CONFIG
+echo "c.ContentsManager.checkpoints_class = 'swancontents.filemanager.checkpoints.EOSCheckpoints'" >> $JPY_CONFIG
 echo "c.NotebookApp.default_url = 'projects'" >> $JPY_CONFIG
 cp -L -r $LCG_VIEW/etc/jupyter/* $JUPYTER_CONFIG_DIR
 
